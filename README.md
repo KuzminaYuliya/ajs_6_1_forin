dc1529d1d2bc7fed5118c91e1073dfde6e545130
KuzminaYuliya <kuzmina_h@mail.ru>
2021-06-27 14:28:04 +0500

    first commit fjr ajs_6_1_forin

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/rav6t74xpp56qvaf?svg=true)](https://ci.appveyor.com/project/KuzminaYuliya/ajs-6-1-forin)
---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+export default function orderByProps(data, order) {
+  const orderSort = [];
+  for (let j = 0; j < order.length; j += 1) {
+    if (order[j] in data) {
+      orderSort.push(order[j]);
+    }
+  }
+  const result = [];
+  for (const prop in data) {
+    if ({}.hasOwnProperty.call(data, prop)) {
+      result.push({ key: prop, value: data[prop] });
+    }
+  }
+  result.sort((prev, next) => (prev.key > next.key ? 1 : -1));
+  for (let i = 0; i < orderSort.length; i += 1) {
+    const index = result.findIndex((item) => item.key === orderSort[i]);
+    result.splice(i, 0, result.splice(index, 1)[0]);
+  }
+  return result;
+}
